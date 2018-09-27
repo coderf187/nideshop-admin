b585532a8384486f85e5a6d327008e92c22c532d
fengjian <976644096@qq.com>
2018-09-27 10:03:23 +0800

    修改配置

--- src/renderer/config/api.js
+++ src/renderer/config/api.js
@@ -1,4 +1,4 @@
-const rootUrl = 'http://127.0.0.1:8360/admin/';
+const rootUrl = 'http://101.200.38.188:8360/admin';
 // const rootUrl = 'http://flyinthesky.cn:8360/admin/';
 const version = 'v0.0.7';
 const isShowDelete = true; // 是否显示删除按钮，上线后的版本不显示
